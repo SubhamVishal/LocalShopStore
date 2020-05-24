6252009b3335a148a296563fce3919bea18e7d2a
subhamvishal <vishalsubham01@gmail.com>
2020-05-24 19:42:17 +0530

    Project Commit

---
+++ store/actions/auth.js
@@ -0,0 +1,145 @@
+import { AsyncStorage } from "react-native";
+
+//export const SIGNUP = "SIGNUP";
+//export const LOGIN = "LOGIN";
+export const AUTHENTICATE = "AUTHENTICATE";
+export const LOGOUT = "LOGOUT";
+
+let timer;
+
+export const authenticate = (userId, token, expiryTime) => {
+  return (dispatch) => {
+    dispatch(setLogoutTimer(expiryTime));
+    dispatch({
+      type: AUTHENTICATE,
+      token,
+      userId,
+    });
+  };
+};
+
+export const signup = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB7y4KrdkG1zEuuiOyRLCsjnYzEi3km20w",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      let message = "Something went wrong";
+      if (errorId === "EMAIL_EXISTS") {
+        message = "This email already exists!";
+      } else if (errorId === "OPERATION_NOT_ALLOWED") {
+        message = "This password is not valid!";
+      } else if (errorId === "TOO_MANY_ATTEMPTS_TRY_LATER") {
+        message = "Your account has been disabled!";
+      }
+      throw new Error(message);
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    //dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
+    dispatch(
+      authenticate(
+        resData.localId,
+        resData.idToken,
+        parseInt(resData.expiresIn) * 1000
+      )
+    );
+    const expirationDate = new Date(
+      new Date().getTime() + parseInt(resData.expiresIn) * 1000
+    );
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+  };
+};
+export const login = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB7y4KrdkG1zEuuiOyRLCsjnYzEi3km20w",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      let message = "Something went wrong";
+      if (errorId === "EMAIL_NOT_FOUND") {
+        message = "This email could not be found!";
+      } else if (errorId === "INVALID_PASSWORD") {
+        message = "This password is not valid!";
+      } else if (errorId === "USER_DISABLED") {
+        message = "Your account has been disabled!";
+      }
+      throw new Error(message);
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    //dispatch({ type: LOGIN, token: resData.idToken, userId: resData.localId });
+    dispatch(
+      authenticate(
+        resData.localId,
+        resData.idToken,
+        parseInt(resData.expiresIn) * 1000
+      )
+    );
+    const expirationDate = new Date(
+      new Date().getTime() + parseInt(resData.expiresIn) * 1000
+    );
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+  };
+};
+
+export const logout = () => {
+  clearLogoutTimer();
+  AsyncStorage.removeItem("userData");
+  return { type: LOGOUT };
+};
+
+const clearLogoutTimer = () => {
+  if (timer) {
+    clearTimeout(timer);
+  }
+};
+
+const setLogoutTimer = (expirationTime) => {
+  return (dispatch) => {
+    timer = setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime);
+  };
+};
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+  AsyncStorage.setItem(
+    "userData",
+    JSON.stringify({
+      userId,
+      token,
+      expiryDate: expirationDate.toISOString(),
+    })
+  );
+};
